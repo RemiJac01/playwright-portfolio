c6d354d16571feb53943ee633b9ba7ba998e0272
RemiJac01 <norburyman@gmail.com>
2026-06-17 23:34:43 +0200

    Refactor checkboxes test to loop through all checkboxes

--- tests/checkboxes.spec.js
+++ tests/checkboxes.spec.js
@@ -1,12 +1,11 @@
-import {test, expect} from '@playwright/test'
+import { test, expect } from "@playwright/test";
 
-test('Checkboxes can be checked and unchecked', async ({page}) => {
-    await page.goto('https://the-internet.herokuapp.com/checkboxes');
-    const checkbox1 = page.getByRole('checkbox').first();
-    await checkbox1.check();
-    await expect(checkbox1).toBeChecked();
-    const checkbox2 = page.getByRole('checkbox').last();
-    await checkbox2.check();
-    await expect(checkbox2).toBeChecked();
-    
-})
+test("Checkboxes can be checked and unchecked", async ({ page }) => {
+  await page.goto("https://the-internet.herokuapp.com/checkboxes");
+  const checkboxes = await page.getByRole("checkbox").all();
+
+  for (const checkbox of checkboxes) {
+    await checkbox.check();
+    await expect(checkbox).toBeChecked();
+  }
+});

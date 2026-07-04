9a1a6e8604eff65b8d022b4d81e962ab61e18a93
RemiJac01 <norburyman@gmail.com>
2026-07-04 14:23:02 +0200

    Add download page file visibility tests

--- tests/download.spec.js
+++ tests/download.spec.js
@@ -1,7 +1,21 @@
 import { test, expect } from "@playwright/test";
 
+const downloadFiles = [
+  { name: "upload_me.txt" },
+  { name: "README.md" },
+  { name: "test_upload_file.txt" },
+  { name: "some-file.txt" },
+  { name: "test-image.jpg" },
+];
+
 test("Download page URL is correct", async ({ page }) => {
   await page.goto("https://the-internet.herokuapp.com/download");
   await expect(page).toHaveURL("https://the-internet.herokuapp.com/download");
-  await expect(page.getByText("vendor.txt")).toBeVisible();
 });
+
+for (const data of downloadFiles) {
+  test(`downloadable file text check ${data.name}`, async ({ page }) => {
+    await page.goto("https://the-internet.herokuapp.com/download");
+    await expect(page.getByRole("link", { name: data.name })).toBeVisible();
+  });
+}

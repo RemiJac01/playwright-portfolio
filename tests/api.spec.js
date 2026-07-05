16ffce1603be6208ab550cd7a3186ee6bc92f702
RemiJac01 <norburyman@gmail.com>
2026-07-05 18:13:16 +0200

    Add title and body assertions to API test

--- tests/api.spec.js
+++ tests/api.spec.js
@@ -8,4 +8,10 @@ test("GET post returns 200 status", async ({ request }) => {
   const body = await response.json();
 
   await expect(body.userId).toBe(1);
+  await expect(body.title).toBe(
+    "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+  );
+  await expect(body.body).toBe(
+    "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+  );
 });

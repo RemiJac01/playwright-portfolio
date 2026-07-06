2dc8fa8f0dd225c95933cef6898c0aa7935fd875
RemiJac01 <norburyman@gmail.com>
2026-07-06 09:30:11 +0200

    Remove flaky download file visibility tests

--- tests/download.spec.js
+++ tests/download.spec.js
@@ -1,18 +1,18 @@
 import { test, expect } from "@playwright/test";
 
-const downloadFiles = [
-  { name: "upload-sample.txt" },
-  { name: "sample-upload.txt" },
-];
+//const downloadFiles = [
+//{ name: "upload-sample.txt" },
+//{ name: "sample-upload.txt" },
+//];
 
 test("Download page URL is correct", async ({ page }) => {
   await page.goto("https://the-internet.herokuapp.com/download");
   await expect(page).toHaveURL("https://the-internet.herokuapp.com/download");
 });
 
-for (const data of downloadFiles) {
-  test(`downloadable file text check ${data.name}`, async ({ page }) => {
-    await page.goto("https://the-internet.herokuapp.com/download");
-    await expect(page.getByRole("link", { name: data.name })).toBeVisible();
-  });
-}
+//for (const data of downloadFiles) {
+//test(`downloadable file text check ${data.name}`, async ({ page }) => {
+//await page.goto("https://the-internet.herokuapp.com/download");
+//await expect(page.getByRole("link", { name: data.name })).toBeVisible();
+//});
+//}
